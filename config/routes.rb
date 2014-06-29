5db0efc435c5c25c1871b423ec176c764455269d
Chad <simpleGrowth@users.noreply.github.com>
2014-06-29 12:19:02 -0400

    Add User model and migrate db

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'user/assets=false'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
