4521309b074aeb328cdca44c166f42237b3d939c
Chad <simpleGrowth@users.noreply.github.com>
2014-06-29 13:11:24 -0400

    User controller configured, welcome view built and create action built

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,21 @@
 class UsersController < ApplicationController
   def welcome
-
+    @user = User.new
+    @users = User.all
   end
 
   def create
-
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path, notice: 'Account created.'
+    else
+      flash.now[:alert] = 'Something bad happened, try again.'
+      render 'welcome'
+    end
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email)
+    end
 end
