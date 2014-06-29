5b7f8e2ca15263c44f9cdeb62c849f7f3e81b8bd
Chad <simpleGrowth@users.noreply.github.com>
2014-06-29 13:45:43 -0400

    Welcome mailer and views created

---
+++ app/mailers/welcome.rb
@@ -0,0 +1,9 @@
+class Welcome < ActionMailer::Base
+  default from: "hello@mysite.com"
+
+  def welcome_email(user)
+    @user = user
+    @url  = 'http://test.com/login'
+    mail(to: @user.email, subject: 'My awesome email')
+  end
+end
