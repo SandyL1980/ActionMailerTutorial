30719d81e1b3fd13a9a19960d04fa5e6bc46750d
Chad <simpleGrowth@users.noreply.github.com>
2014-06-29 14:35:26 -0400

    Bug fix, rename my Mailer

--- app/mailers/welcome_mailer.rb
+++ app/mailers/welcome_mailer.rb
@@ -1,9 +1,10 @@
-class Welcome < ActionMailer::Base
+class WelcomeMailer < ActionMailer::Base
   default from: "hello@mysite.com"
 
   def welcome_email(user)
     @user = user
     @url  = 'http://test.com/login'
     mail(to: @user.email, subject: 'My awesome email')
+    # pry
   end
 end
